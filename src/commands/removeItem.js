9f336b1bf941db4aeb3aa90cf9a0e2f989bc0ce3
yungestt <wertypro1230@gmail.com>
2025-07-30 22:29:55 +0300

    Update removeItem.js

--- src/commands/removeItem.js
+++ src/commands/removeItem.js
@@ -1,1 +1,32 @@
-.
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { User, ShopItem } = require("../db/models");
+const { removeItem } = require("../config/messagesConfig.json");
+const Mustache = require("mustache");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName(removeItem.commandName)
+    .setDescription(removeItem.commandDescription)
+    .addIntegerOption((option) =>
+      option
+        .setName(removeItem.idName)
+        .setDescription(removeItem.idDescription)
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    await interaction.deferReply();
+    const id = interaction.options.getInteger(removeItem.idName);
+    try {
+      await ShopItem.destroy({
+        where: { id },
+        force: true,
+      });
+
+      await interaction.editReply(removeItem.replyText);
+    } catch (error) {
+      console.error(error);
+      await interaction.editReply(removeItem.errorText);
+    }
+  },
+};
