bf6507606af4a9590df9218e018cb0ce9ad6646b
yungestt <wertypro1230@gmail.com>
2025-08-01 11:09:01 +0300

    Update deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,29 +1,23 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
+const { Routes } = require("discord-api-types/v10");
 require("dotenv").config();
 
-const rest = new REST({ version: "9" }).setToken(process.env.TOKEN);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 async function deployCommands(commands) {
-  const commandData = commands.map((command) => command.data.toJSON());
+  const body = commands.map(cmd => cmd.data.toJSON());
 
   try {
-    console.log("Started refreshing application (/) commands.");
+    console.log("🔄 Регистрируем команды...");
 
     await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
-      {
-        body: commandData,
-      }
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body }
     );
 
-    console.log("Successfully reloaded application (/) commands.");
+    console.log("✅ Команды зарегистрированы!");
   } catch (error) {
-    console.error(error);
+    console.error("❌ Ошибка при регистрации команд:", error);
   }
 }
 
