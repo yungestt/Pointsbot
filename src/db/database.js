ee2149860ea58722274f6f6bf69870506557830d
yungestt <wertypro1230@gmail.com>
2025-08-01 11:08:16 +0300

    Update database.js

--- src/db/database.js
+++ src/db/database.js
@@ -1,32 +1,5 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
-);
-
 async function syncDB() {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    await sequelize.sync({ force: false });
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+  console.log("📦 Database disabled (no sync).");
 }
 
-module.exports = { sequelize, syncDB };
+module.exports = { syncDB };
